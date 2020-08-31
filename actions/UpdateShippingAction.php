004bea6d21c579cc9e1c34182ecfa4e28403c141
Laion <leonardo.navarro@coodesoft.com.ar>
2020-08-31 20:31:05 -0300

    se agregó soporte para que la actualización de un envío incluya la actualización de sus items. Solo actualización de la descripción

---
+++ actions/UpdateShippingAction.php
@@ -0,0 +1,62 @@
+<?php
+namespace app\actions;
+
+use Yii;
+use yii\rest\UpdateAction;
+use yii\helpers\Url;
+use app\models\ShippingItem;
+
+class UpdateShippingAction extends UpdateAction {
+
+    public function run($id) {
+      $response = Yii::$app->getResponse();
+      $model = $this->modelClass::find()->where(['id' => $id])->one();
+      $params = Yii::$app->getRequest()->getBodyParams();
+      $model->load($params, '');
+      $anyErrors = false;
+      $errorMsg = '';
+
+      try {
+        $transaction = ShippingItem::getDb()->beginTransaction();
+        if ( $model->save() ){
+          if ( isset($params['items']) && !empty($params['items']) ) {
+            foreach ($params['items'] as $key => $item) {
+              $storedItem = ShippingItem::find()->where(['id' => $item['id']])->one();
+              if ( $storedItem != null ){
+
+                $storedItem->item = $item['description'];
+                if ( !$storedItem->save() ){
+                  $anyErrors = true;
+                  $errorMsg = $storedItem->getErrors();
+                  break;
+                }
+              }
+            }
+          }
+
+          if ($anyErrors){
+            $transaction->rollBack();
+            $response->statusCode(400);
+            $response->format = \yii\web\Response::FORMAT_JSON;
+            $response->data = [ 'status' => false, 'message' => $errorMsg ];
+          } else{
+            $transaction->commit();
+            $response->setStatusCode(200);
+            $response->data = [ 'status' => true ];
+          }
+
+        } elseif ( $model->hasErrors() ){
+          $response->setStatusCode(400);
+          $response->format = \yii\web\Response::FORMAT_JSON;
+          $response->data = [ 'status' => false, 'message' => $model->getErrors() ];
+        } else {
+            throw new ServerErrorHttpException('Failed to create the object for unknown reason.');
+        }
+
+      } catch (\Throwable $e) {
+        $response->setStatusCode(400);
+        $response->format = \yii\web\Response::FORMAT_JSON;
+        $response->data = [ 'message' => $e->getMessage() ];
+      }
+    }
+}
