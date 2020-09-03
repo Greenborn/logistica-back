5bc5bc86d55acc469f2d08c8a719ef1a55f23229
Laion <leonardo.navarro@coodesoft.com.ar>
2020-09-03 12:05:40 -0300

    se estandarizó la respuesta ante la acción de login

--- actions/LoginAction.php
+++ actions/LoginAction.php
@@ -14,16 +14,24 @@ class LoginAction extends CreateAction {
       $username = isset($params['username']) ? $params['username'] : null;
       $password = isset($params['password']) ? $params['password'] : null;
 
+      $response = Yii::$app->getResponse();
+      $response->format = \yii\web\Response::FORMAT_JSON;
+      $status = false;
+
       if ($username && $password){
         $user = User::find()->where( ['username' => $username] )->one();
-        if ($user){
-        //  return Yii::$app->getSecurity()->generatePasswordHash($password);
-        //  return Yii::$app->getSecurity()->validatePassword($password, $user->password_hash);
-          if (Yii::$app->getSecurity()->validatePassword($password, $user->password_hash)) {
-            return $user->access_token;
-        }
+        $status = $user && Yii::$app->getSecurity()->validatePassword($password, $user->password_hash);
       }
-      return false;
-    }
+
+    if ($status)
+        $response->data = [
+          'status' => 'success',
+          'token'  => $user->access_token,
+        ];
+    else
+      $response->data = [
+          'status' => 'failure',
+          'message' => 'Unauthorize Access!',
+      ];
   }
 }
