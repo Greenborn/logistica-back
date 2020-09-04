9d49317badb1de5a1482036ec353651789ae2c39
Laion <leonardo.navarro@coodesoft.com.ar>
2020-09-04 00:32:23 -0300

    endpoint de status

---
+++ controllers/ShippingStatusController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\data\ActiveDataProvider;
+use app\models\ShippingType;
+
+class ShippingStatusController extends BaseController {
+
+    public $modelClass = 'app\util\Status';
+
+    public function actions(){
+      $actions = parent::actions();
+      unset( $actions['delete'],
+             $actions['update'],
+             $actions['create'],
+             $actions['view'],
+           );
+
+      $actions['index']['class'] = 'app\actions\StatusIndexAction';
+
+      return $actions;
+    }
+
+    public function actionIndex() {
+      return new ActiveDataProvider([
+        'query' => ShippingType::find(),
+      ]);
+    }
+}
